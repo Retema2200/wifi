e253b7dca5ea86f28518a7d1ef5b73dbe9346cdf
Retema2200 <s125145757@gmail.com>
2024-12-03 16:55:15 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "temperature-monitor-cache-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./styles.css",
+    "./manifest.json",
+    "./app.js"
+];
+
+// 安裝 Service Worker
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
